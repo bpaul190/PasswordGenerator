7a28613c91d482af30b5d617d45aa213909af3e7
bpaul190 <bfp4190@gmail.com>
2020-07-29 17:41:35 +0000

    first commit

---
+++ model.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+letters = string.ascii_letters
+numbers = string.digits
+punctuation = string.punctuation
+
+print(letters)
+print(numbers)
+#def get_password(numcharacters, capital, numbers, specialchrs):
+
